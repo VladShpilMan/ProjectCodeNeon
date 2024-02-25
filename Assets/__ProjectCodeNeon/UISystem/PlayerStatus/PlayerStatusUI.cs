451e1ebbfdfa958418961493f941c8feb0bc8c3d
Waulesi <85766091+IntCat13@users.noreply.github.com>
2024-02-25 10:50:06 +0100

    Add player status controller

---
+++ Assets/__ProjectCodeNeon/UISystem/PlayerStatus/PlayerStatusUI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStatusUI : MonoBehaviour
+{
+    [SerializeField] private Image _hp;
+    
+    void Start()
+    {
+        
+    }
+
+    
+}
